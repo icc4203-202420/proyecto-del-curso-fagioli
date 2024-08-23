9dc734aafc7705c67325cef6b9cd31298bfc57a0
LucianoBaldoFagioli <lbfagioli@miuandes.cl>
2024-08-23 12:33:19 -0400

    feat: implement events and friendships controller with routs

---
+++ backend/app/controllers/api/v1/friendships_controller.rb
@@ -0,0 +1,45 @@
+class API::V1::FriendshipsController < ApplicationController
+    include Authenticable
+
+    respond_to :json
+    before_action :verify_jwt_token, only: [:index, :create]
+    before_action :set_user
+    
+    def index
+      @friendships = Friendship.all   
+      render json: { friendships: @friendships }, status: :ok
+    end
+  
+    def show
+    
+    end
+  
+    def create
+        @friendship = Friendship.new(friendship_params.merge(user_id: @user.id))
+        if @friendship.save
+            render json: { friendship: @friendship, message: "Friendship done" }, status: :ok
+        else
+            render json: @friendship.errors, status: :unprocessable_entity
+        end
+    end
+  
+    def update
+        
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def friendship_params
+        params.require(:friendship).permit(:friend_id, :bar_id)
+    end
+
+    def verify_jwt_token
+        authenticate_user!
+        head :unauthorized unless current_user
+    end
+  end
+  
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -25,9 +25,11 @@ Rails.application.routes.draw do
       resources :beers
       resources :users do
         resources :reviews, only: [:index]
+        resources :friendships, only: [:index, :create]
       end
-      # new change
+      
       resources :reviews, only: [:index, :show, :create, :update, :destroy]
+      resources :events, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
